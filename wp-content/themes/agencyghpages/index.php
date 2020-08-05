035484adb24d39c5be71ee7ef145c7acd8f7e060
Jean-PierrePapin <skouo82@gmail.com>
2020-08-05 18:19:16 +0100

    Began template parts

--- wp-content/themes/agencyghpages/index.php
+++ wp-content/themes/agencyghpages/index.php
@@ -1,14 +1,15 @@
 <?php 
 
 /**
- * 
+ * Index Page for the Agency - Start Bootstrap Theme
  */
 
-?>
+get_header();
 
-<?php get_header() ?>
+get_template_part( 'template-parts/content', 'services' );
+get_template_part( 'template-parts/content', 'portfolio' );
+get_template_part( 'template-parts/content', 'about' );
+get_template_part( 'template-parts/content', 'team' );
+get_template_part( 'template-parts/content', 'contact' );
 
-
-
-
-<?php get_footer() ?>
+get_footer();
