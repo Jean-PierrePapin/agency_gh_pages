0ff9c35d4c646e97486f911bf05ebca6db037036
Jean-PierrePapin <skouo82@gmail.com>
2020-09-01 11:53:50 +0100

    Installed the WP Form plugin

--- wp-content/themes/agencyghpages/template-parts/content-contact.php
+++ wp-content/themes/agencyghpages/template-parts/content-contact.php
@@ -9,12 +9,15 @@
 <!-- Contact-->
 <section class="page-section" id="contact">
     <div class="container">
+
         <div class="text-center">
             <h2 class="section-heading text-uppercase">Contact Us</h2>
             <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
         </div>
+
         <form id="contactForm" name="sentMessage" novalidate="novalidate">
             <div class="row align-items-stretch mb-5">
+
                 <div class="col-md-6">
                     <div class="form-group">
                         <input class="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name." />
@@ -29,17 +32,22 @@
                         <p class="help-block text-danger"></p>
                     </div>
                 </div>
+
                 <div class="col-md-6">
                     <div class="form-group form-group-textarea mb-md-0">
                         <textarea class="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
                         <p class="help-block text-danger"></p>
                     </div>
                 </div>
+                
             </div>
+
             <div class="text-center">
                 <div id="success"></div>
                 <button class="btn btn-primary btn-xl text-uppercase" id="sendMessageButton" type="submit">Send Message</button>
             </div>
+
         </form>
+
     </div>
 </section>
